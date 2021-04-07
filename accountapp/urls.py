391c1df3ac8c774d9bf7b6a062e702639858c4cb
developer-Wonjun <iwonjun97@naver.com>
2021-04-07 22:34:21 +0900

    add user_detail_view

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from .views import hello_world, AccountCreateView
+from .views import hello_world, AccountCreateView, AccountDetailView
 
 app_name = "accountapp"
 
@@ -11,4 +11,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from .models import HelloWorld
 # Create your views here.
@@ -30,3 +30,8 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
