418e55a403ef2cd84635db8afc1eff3a2338f7c6
developer-Wonjun <iwonjun97@naver.com>
2021-04-07 19:28:06 +0900

    add sign up

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from .models import HelloWorld
 # Create your views here.
@@ -22,3 +25,8 @@ def hello_world(request):
 
 
 
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_word')
+    template_name = 'accountapp/create.html'
