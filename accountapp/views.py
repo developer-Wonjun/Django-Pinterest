b52d0ed08fbf5cf7cdeac8c984008e623b7d5d1d
developer-Wonjun <iwonjun97@naver.com>
2021-04-07 20:01:10 +0900

    add login/logout

--- accountapp/views.py
+++ accountapp/views.py
@@ -28,5 +28,5 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_word')
+    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 import os, environ
+
+from django.urls import reverse_lazy
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -135,3 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
