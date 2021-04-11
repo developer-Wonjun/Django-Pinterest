d9b4efd6ff9508a4d1a1c61bb1c7e3afb0fcc0fc
developer-Wonjun <iwonjun97@naver.com>
2021-04-11 23:22:34 +0900

    add profile

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form): #form에 유저정보가 담겨있지않아서 오류가뜸. 따라서 지정해주는 함수
+        temp_profile = form.save(commit=False)
+        temp_profile.user = self.request.user
+        temp_profile.save()
+
+        return super().form_valid(form)
