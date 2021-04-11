beb086e4aec0a97ab588b104903929a42fe280eb
developer-Wonjun <iwonjun97@naver.com>
2021-04-11 16:48:39 +0900

    user authenticated

--- accountapp/views.py
+++ accountapp/views.py
@@ -11,19 +11,23 @@ from .models import HelloWorld
 
 def hello_world(request):
 
-    if request.method == "POST":
+    if request.user.is_authenticated:
 
-        temp = request.POST.get('hello_world_input')
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
+        if request.method == "POST":
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            temp = request.POST.get('hello_world_input')
+
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
+
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/helloworld.html', context={'hello_world_list':hello_world_list})
     else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/helloworld.html', context={'hello_world_list':hello_world_list})
-
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 
 class AccountCreateView(CreateView):
@@ -41,14 +45,12 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
     context_object_name = 'target_user'
-
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
 
