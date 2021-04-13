4b724520b0c78fdee50009cd4759c92abbe1277d
developer-Wonjun <iwonjun97@naver.com>
2021-04-13 20:09:59 +0900

    프로필 기능 구현

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#이래야 서버가 사진을 뱉어냄
+
---
+++ profileapp/decorators.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponseForbidden
+
+from profileapp.models import Profile
+
+
+def profile_ownership_required(func):
+    def decorated(request, *args, **kwargs):
+        profile = Profile.objects.get(pk=kwargs['pk'])
+
+        if not profile.user == request.user:
+            return HttpResponseForbidden()
+        return func(request, *args, **kwargs)
+
+    return decorated
