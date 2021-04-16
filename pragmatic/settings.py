ac18c563240fffa7950e1160aadfa8720c639b27
developer-Wonjun <iwonjun97@naver.com>
2021-04-16 17:04:27 +0900

    comment앱 생성 및 모델링/form생성

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     'profileapp',
     'articleapp',
+    'commentapp',
 ]
 
 MIDDLEWARE = [
--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
+    path('comments/', include('commentapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#이래야 서버가 사진을 뱉어냄
 
