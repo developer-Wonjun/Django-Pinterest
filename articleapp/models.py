ef258a33c057023f73a3ecf125c07625c6cfd713
developer-Wonjun <iwonjun97@naver.com>
2021-04-17 19:20:02 +0900

    project - article & user - article 연결

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,10 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
 
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)#게시글삭제돼도글남음
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)#게시글삭제돼도글남음
+
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=False)
     content = models.TextField(null=True)
--- projectapp/views.py
+++ projectapp/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.list import MultipleObjectMixin
 
+from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
@@ -19,10 +21,16 @@ class ProjectCreateView(CreateView):
     def get_success_url(self):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(DetailView, MultipleObjectMixin): #여러가지 오브젝트를 다룰 수 있는 mixin
     model = Project
     template_name = 'projectapp/detail.html'
     context_object_name = 'target_project'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs): #어떤 객체를 가져올지.
+        object_list = Article.objects.filter(project=self.get_object())
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 class ProjectListView(ListView):
     model = Project
