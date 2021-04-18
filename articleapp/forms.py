73e82f8127290749746ca45235981b7bae87ee85
developer-Wonjun <iwonjun97@naver.com>
2021-04-18 19:50:55 +0900

    Article forms 수정 (글 작성 시 글씨 형태)

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,16 @@
 from django.forms import ModelForm
-
+from django import forms
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'height:auto; text-align:left;'}))
+
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
+
     class Meta:
         model = Article
         fields = ['title', 'project', 'image',  'content']
