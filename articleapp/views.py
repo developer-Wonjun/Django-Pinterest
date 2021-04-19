70e17c7f0933fcb8ff3eb3957a0c74a2eb055470
developer-Wonjun <iwonjun97@naver.com>
2021-04-19 22:05:29 +0900

    메인화면 템플릿 수정

--- articleapp/views.py
+++ articleapp/views.py
@@ -61,4 +61,4 @@ class ArticleListView(ListView):
     model = Article
     context_object_name = 'article_list'
     template_name = 'articleapp/list.html'
-    paginate_by = 20
+    paginate_by = 10
